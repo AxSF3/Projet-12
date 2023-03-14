c9e730c3fd198c2c3cbe60513ac0d03c9eb5473e
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2023-03-14 17:19:41 +0100

    Add files via upload

--- Client/src/components/styles/404.styled.js
+++ Client/src/components/styles/404.styled.js
@@ -3,24 +3,27 @@ import styled from "styled-components";
 export const Styled404 = styled.section`
 
 &.page-404 {
-    text-align: center
+    text-align: center;
   }
   
   .titre-404 {
     color: #ff6060;
     font-size: 150px;
     margin: auto;
+    margin-left: 500px
   }
   
   .description-404 {
     color: #ff6060;
     display: block;
     font-size: 20px;
+    margin-left: 500px
   }
   
   .lien-site.lien-404 {
     display: block;
     margin-top: 150px;
+    margin-left: 500px
   }
   
   @media screen and (max-width: 1490px) {
